4216df70696864bd0bc935b52f7b556296df382c
thiago9800 <57380109+thiago9800@users.noreply.github.com>
2021-05-17 12:00:57 -0300

    Update button-function.js

--- button-function.js
+++ button-function.js
@@ -1,35 +1,35 @@
 function MudarCorBranco()
     {
         //alert("botão Branco pressionado");
-        document.getElementById('quadrado').style.background = "white";   
+        document.getElementById('quadrado').style.background = "white"; //localiza o id 'quadrado' no html para alterar a sua cor para branco no css
     } 
 
 function MudarCorVermelho()
     {
         //alert("botão Vermelho pressionado");
-        document.getElementById('quadrado').style.background = "red";
+        document.getElementById('quadrado').style.background = "red"; //localiza o id 'quadrado' no html para alterar a sua cor para vermelho no css
      } 
 
 function MudarCorVerde()
     {
         //alert("botão Verde pressionado");
-        document.getElementById('quadrado').style.background = "green";   
+        document.getElementById('quadrado').style.background = "green"; //localiza o id 'quadrado' no html para alterar a sua cor para verde no css  
     } 
 
 function MudarCorAzul()
     {
         //alert("botão Azul pressionado");
-        document.getElementById('quadrado').style.background = "blue";   
+        document.getElementById('quadrado').style.background = "blue"; //localiza o id 'quadrado' no html para alterar a sua cor para azul no css 
     } 
 
 function MudarCorPreto()
     {
         //alert("botão preto pressionado");
-        document.getElementById('quadrado').style.background = "black";   
+        document.getElementById('quadrado').style.background = "black"; //localiza o id 'quadrado' no html para alterar a sua cor para preto no css 
     } 
 
-        var button1 = document.getElementById("button1");
-        button1.addEventListener("click", MudarCorBranco);
+        var button1 = document.getElementById("button1"); // Retorna a referência do elemento através do seu ID. 
+        button1.addEventListener("click", MudarCorBranco); // O método addEventListener () anexa um manipulador de eventos ao elemento especificado. ou comfirma q o botão foi presionado e inicia a function MudarCorBranco
 
         var button2 = document.getElementById("button2");
         button2.addEventListener("click", MudarCorVermelho);
